9238ed8be7854a21f85ebaa33b2caccd691bc62e
andreszj <cazuniga02@gmail.com>
2025-03-29 15:42:50 -0600

    Added Exercise 2

--- Semana_12/Ejercicio_1.py
+++ Semana_12/Ejercicio_1.py
@@ -3,11 +3,10 @@ class BankAccount:
 
     def add_money (self, amount):
         self.balance += amount
-        print (f'New balance: {self.balance}')
+        print (f'Balance: {self.balance}')
     
-    def subtract_money (self, amount):
+    def _subtract_money (self, amount):
         self.balance -= amount
-        return self.balance
 
 
 class SavingsAccount (BankAccount):
@@ -16,14 +15,15 @@ class SavingsAccount (BankAccount):
         self.min_balance = min_balance
     def take_out_money (self, amount):
         try:
-            if (self.subtract_money (amount) < self.min_balance):
+            if (self.balance  - amount < self.min_balance):
                 raise ValueError()
             else:
-                self.subtract_money (amount)
-                print (f'New balance: {self.balance}')
-        except ValueError as error:
+                self._subtract_money (amount)
+                print (f'Balance: {self.balance}')
+        except ValueError:
             print (f'If you withdraw money, your balance would fall below the minimum')
+            print (f'Balance: {self.balance}')
 
 account = SavingsAccount(1500)
-account.add_money (1000)
+account.add_money (2000)
 account.take_out_money (100)
