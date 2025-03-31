0113f85aefe4e2fcb0e9e4e4bf4f75958b44acab
andreszj <cazuniga02@gmail.com>
2025-03-31 17:21:31 -0600

    Adding requested changes

--- Semana_12/Ejercicio_1.py
+++ Semana_12/Ejercicio_1.py
@@ -1,24 +1,29 @@
 class BankAccount:
-    balance = 0
+
+    # balance = 0
+
+    def __init__ (self):
+        self.balance = 0
 
     def add_money (self, amount):
         self.balance += amount
         print (f'Balance: {self.balance}')
     
-    def _subtract_money (self, amount):
+    def subtract_money (self, amount):
         self.balance -= amount
 
 
 class SavingsAccount (BankAccount):
 
     def __init__(self, min_balance):
+        super().__init__()
         self.min_balance = min_balance
     def take_out_money (self, amount):
         try:
             if (self.balance  - amount < self.min_balance):
                 raise ValueError()
             else:
-                self._subtract_money (amount)
+                self.subtract_money (amount)
                 print (f'Balance: {self.balance}')
         except ValueError:
             print (f'If you withdraw money, your balance would fall below the minimum')
